6667b6ab8a2770ad23277693ca609fa423b5a102
Hanif Muhammad <user@MacBook-Air.local>
2026-06-07 15:59:16 +0700

    prepare deployment

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://admin:admin123@localhost:5432/fraud_db"
+import os
+
+DATABASE_URL = os.environ["DATABASE_URL"]
 
 engine = create_engine(DATABASE_URL)
 
